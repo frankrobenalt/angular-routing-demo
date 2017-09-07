b648e86199f53ba62294cd7ff3988a9d9e781341
Frank Robenalt <frank.robenalt@gmail.com>
2017-09-07 11:25:15 -0500

    done

--- js/app.routes.js
+++ js/app.routes.js
@@ -1,3 +1,29 @@
-angular.module('routing').config(function() {
-
-});
+angular.module('routing').config(function($stateProvider, $urlRouterProvider) {
+    
+      $urlRouterProvider.otherwise('/');
+    
+      $stateProvider
+        .state('home', {
+          templateUrl: '../views/home.html',
+          url: '/',
+          controller: 'homeCtrl'
+        })
+        .state('characters', {
+          url: '/characters',
+          controller: 'characterCtrl',
+          templateUrl: '../views/characters.html',
+          // because we want to retrieve all characters in the ng-repeat
+          resolve: {
+            characters: function(characterSrvc) {
+              return characterSrvc.getCharacters().then(function(response){
+                return response;
+              });
+            }
+          }
+        })
+        .state('character', {
+          url: '/character/:charID',
+          controller: 'detailsCtrl',
+          templateUrl: '../views/details.html'
+        })
+    });
